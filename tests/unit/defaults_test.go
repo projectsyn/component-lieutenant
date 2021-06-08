cda5e0e21911c374c75bb9bc6927db26c51b46d7
Fabian Fischer <glrf@users.noreply.github.com>
2021-06-08 16:27:38 +0200

    Add k8s resource validation
    We are using kubeval to validate the generated resources against
    the k8s OpenAPI specification. This should catch quite a lot of
    errors.

--- tests/unit/defaults_test.go
+++ tests/unit/defaults_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/instrumenta/kubeval/kubeval"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -15,6 +16,52 @@ import (
 var (
 	testPath = "../../compiled/lieutenant/lieutenant"
 )
+
+// kubeval is unable to validate some of the resources
+// we need to explicitly ignore them
+func skipValidation(path string) bool {
+	ignore := []string{
+		fmt.Sprintf("%s/00_crds", testPath),
+	}
+	for _, iv := range ignore {
+		if iv == path {
+			return true
+		}
+	}
+	return false
+}
+
+func validate(t *testing.T, path string) {
+	files, err := ioutil.ReadDir(path)
+	require.NoError(t, err)
+	for _, file := range files {
+		filePath := fmt.Sprintf("%s/%s", path, file.Name())
+		if skipValidation(filePath) {
+			continue
+		}
+		if file.IsDir() {
+			validate(t, filePath)
+		} else {
+			data, err := ioutil.ReadFile(filePath)
+			require.NoError(t, err)
+
+			conf := kubeval.NewDefaultConfig()
+			res, err := kubeval.Validate(data, conf)
+			require.NoError(t, err)
+			for _, r := range res {
+        if len(r.Errors) > 0 {
+					t.Errorf("%s", filePath)
+        }
+				for _, e := range r.Errors {
+					t.Errorf("\t %s", e)
+				}
+			}
+		}
+	}
+}
+func Test_Validate(t *testing.T) {
+	validate(t, testPath)
+}
 
 func Test_CRDs(t *testing.T) {
 	crds := []string{
